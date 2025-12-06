de52b66d654ee66f15f97110e6a49c7ada8215d9
devopskl08 <likhithareddyk0815@gmail.com>
2025-12-06 16:22:42 +0530

    Update firstPipeline.groovy

--- var/firstPipeline.groovy
+++ var/firstPipeline.groovy
@@ -9,7 +9,7 @@ def call(Map pipelineParams) {
         label 'slave-1'
     }
      environment {
-        APPLICATION_NAME = ${"pipelineParams.appName"}
+        APPLICATION_NAME = "${pipelineParams.appName}"
      }
     stages{
         stage('Addition'){
@@ -30,3 +30,4 @@ def call(Map pipelineParams) {
   }
 
 }
+
